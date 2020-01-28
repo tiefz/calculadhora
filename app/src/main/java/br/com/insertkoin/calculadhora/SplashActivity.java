58fa6a99bda4be4c2a58dcca5e08ce2832d3063d
Daniel Tief <chinorex@gmail.com>
2020-01-27 22:10:05 -0300

    Implementação inicial welcome screen

--- app/src/main/java/br/com/insertkoin/calculadhora/SplashActivity.java
+++ app/src/main/java/br/com/insertkoin/calculadhora/SplashActivity.java
@@ -19,7 +19,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getBaseContext(), MainActivity.class));
+                startActivity(new Intent(getBaseContext(), WelcomeActivity.class));
                 finish();
             }
         }, 1000);
