2c69dbdb85a5ad6a233207f3feb750eb2b42490a
Daniel Tiefenbarher <chinorex@gmail.com>
2021-08-13 00:23:23 -0300

    Implementado dataBinding e SplashScreen

--- app/src/main/java/br/com/insertkoin/calculadhora/ui/SplashScreen.kt
+++ app/src/main/java/br/com/insertkoin/calculadhora/ui/SplashScreen.kt
@@ -1,11 +1,12 @@
-package br.com.insertkoin.calculadhora
+package br.com.insertkoin.calculadhora.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.insertkoin.calculadhora.R
 
-class MainActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
     }
 }
